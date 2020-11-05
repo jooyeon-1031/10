1e153ec9119681c5240dd417837b1694e55e5383
jooyeon-1031 <jmhjoin@naver.com>
2020-11-05 16:28:30 +0900

    06

--- main.c
+++ main.c
@@ -6,16 +6,23 @@
 int main(int argc, char *argv[]) {
  int i;
  int grade[5];
+ int *ptr;
+ int sum=0;
  
  for (i=0;i<5;i++)
 {
+	ptr = grade+i;
 	 printf("grade[%i] = ", i);
-	 scanf("%d", &grade[i]);
+	 scanf("%d", ptr);
 }
 
 for (i=0;i<5;i++)
 {
+	ptr = &grade[i];
+	sum += *ptr;
 	 printf("grade[%d] = %d\n", i, grade[i]);
 }
+
+printf("average : %i\n", sum/5);
 return 0;
 }
